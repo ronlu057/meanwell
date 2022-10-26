64474c3f2a780836520c4d5eac2d06b0b9aaa61b
ronlu057 <ronlu057@gmail.com>
2022-10-26 16:51:19 +0800

    26四點50

--- src/views/MemberCentre.vue
+++ src/views/MemberCentre.vue
@@ -1,7 +1,52 @@
 <template>
     <main>
         <div class="page_content">
-            <div class="PageBreadCrumbs"></div>
+            <div class="PageBreadCrumbs">
+                <ul>
+                    <li>首頁</li>
+                    <li>></li>
+                    <li>會員中心</li>
+                </ul>
+            </div>
+            <section class="MemberCentreContent">
+                <div class="memberNav">
+                    <div class="memberNavItem active">
+                        <div class="navItems">
+                            <div class="memberNavItemLine"></div>
+                            <div class="memberNavItemtext">SDG帳戶</div>
+                        </div>
+                    </div>
+                    <div class="memberNavItem">
+                        <div class="navItems">
+                            <div class="memberNavItemLine"></div>
+                            <div class="memberNavItemtext">帳號管理</div>
+                        </div>
+                        <div class="navIcon"></div>
+                    </div>
+                    <div class="memberNavItem">
+                        <div class="navItems">
+                            <div class="memberNavItemLine"></div>
+                            <div class="memberNavItemtext">我的活動</div>
+                        </div>
+                        <div class="navIcon"></div>
+                    </div>
+                    <div class="memberNavItem">
+                        <div class="navItems">
+                            <div class="memberNavItemLine"></div>
+                            <div class="memberNavItemtext">領取分票</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="memberCenterRight">
+                    <div class="memberCenterRightTopBox">
+                        <div class="memberUserBox">
+                            <div class="memberUserBoxLeft"><img src="../img/memberUserIcon.svg" alt=""></div>
+                        </div>
+                        <div class="memberUserQRcord"></div>
+                    </div>
+                    <div class="memberCenterRightContentBox"></div>
+                </div>
+            </section>
         </div>
     </main>
 </template>
