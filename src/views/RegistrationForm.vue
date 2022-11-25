a3574f112e8d204f3654eb6ea371fa563e6002b4
ronlu057 <ronlu057@gmail.com>
2022-11-25 18:02:27 +0800

    1125

--- src/views/RegistrationForm.vue
+++ src/views/RegistrationForm.vue
@@ -3,11 +3,22 @@
     import formDatas from '../stores/datasheet.json'
     
     const fromdata = ref(formDatas)
-    onMounted(()=>{
-        fromdata.value
-        console.log( fromdata )
-    })
+    const entername = ref('')
+    const chooseide = ref(0)
+    const chooseMet = ref(0)
+    const chooseGen = ref(0)
 
+    const chooseidentity = (checkedide) => {
+        chooseide.value =  checkedide
+        console.log( chooseide.value )
+    }
+    const chooseMethod = (checkedMet) => {
+        chooseMet.value =  checkedMet
+        console.log( chooseMet.value )
+    }
+    const choosegender = (checkedGen) => {
+        chooseGen.value = checkedGen
+    }
 </script>
 <template>
 <div class="page_main">
@@ -26,44 +37,44 @@
                 </ul>
             </div>
             <div class="CourseContentBox">
-                <div class="CourseinfTitle">{{ fromdata.eventRegistration.eventtitle }}</div>
-                <div class="activitiesSubtitle">{{ fromdata.eventRegistration.RegistrationInformation }}</div>
+                <div class="CourseinfTitle">{{ fromdata.eventR.eventtitle }}</div>
+                <div class="activitiesSubtitle">{{ fromdata.eventR.RegistrationInformation }}</div>
                 <div class="itemTitle">
                     <div class="itemTitleLine"></div>
-                    <div class="itemTitletext">參與身分</div>
+                    <div class="itemTitletext">{{fromdata.eventR.participatingStatus}}</div>
                 </div>
                 <div class="memberinfTextinput">
-                    <input id="identity01" type="radio" name="identity" checked="checked">
-                    <label for="identity01">員工</label>
-                    <input id="identity02" type="radio" name="identity">
-                    <label for="identity02">外賓</label>
-                </div>
-                <div class="itemTitle">
-                    <div class="itemTitleLine"></div>
-                    <div class="itemTitletext">與會方式</div>
-                </div>
-                <div class="memberinfTextinput">
-                    <input id="mode01" type="radio" name="mode" checked="checked">
-                    <label for="mode01">親臨</label>
-                    <input id="mode02" type="radio" name="mode">
-                    <label for="mode02">線上zoom meeting</label>
+                    <div class="inputitem" v-for="(identity,checkedide) in fromdata.eventR.position" :key="identity.id" @click="chooseidentity(checkedide)">
+                        <input :id="identity.name" type="radio" :name="identity.position" :value="identity.values" v-model="chooseide">
+                        <label :for="identity.name">{{ identity.thisname }}</label>
+                    </div>
                 </div>
                 <div class="itemTitle">
                     <div class="itemTitleLine"></div>
-                    <div class="itemTitletext">姓名</div>
+                    <div class="itemTitletext">{{ fromdata.eventR.ParticipationMethod }}</div>
                 </div>
                 <div class="memberinfTextinput">
-                    <input type="text" name="" id="" class="memberinfinput" placeholder="王小明">
+                    <div class="inputitem" v-for="(Method,checkedMet) in fromdata.eventR.RFMethods" :key="Method.id" @click="chooseMethod(checkedMet)">
+                        <input :id="Method.name" type="radio" :name="Method.position" :value="Method.values" v-model="chooseMet">
+                        <label :for="Method.name">{{ Method.thisname }}</label>
+                    </div>
                 </div>
                 <div class="itemTitle">
                     <div class="itemTitleLine"></div>
-                    <div class="itemTitletext">性別</div>
+                    <div class="itemTitletext">{{fromdata.eventR.CMname}}</div>
                 </div>
                 <div class="memberinfTextinput">
-                    <input id="gender01" type="radio" name="gender" checked="checked">
-                    <label for="gender01">男</label>
-                    <input id="gender02" type="radio" name="gender">
-                    <label for="gender02">女</label>
+                    <input type="text" name="" id="" class="memberinfinput" :placeholder="fromdata.eventR.PlCMname" v-model="entername">
+                </div>
+                <div class="itemTitle">
+                    <div class="itemTitleLine"></div>
+                    <div class="itemTitletext">{{ fromdata.eventR.gender }}</div>
+                </div>
+                <div class="memberinfTextinput">
+                    <div class="inputitem"  v-for="(gender,checkedGen) in fromdata.eventR.sex" :key="gender.id" @click="choosegender(checkedGen)">
+                        <input :id="gender.name" type="radio" :name="gender.position" :value="gender.values" v-model="chooseGen">
+                        <label :for="gender.name">{{ gender.thisname }}</label>
+                    </div>
                 </div>
                 <div class="itemTitle">
                     <div class="itemTitleLine"></div>
